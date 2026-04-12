bae1663306963ddc5fa09edd0219bc33f0e0d64f
vickie25 <nyandorovictor3900@gmail.com>
2026-04-12 17:07:54 +0300

    ADD: attendee object and functions for logging and updating ticket details

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 //Write your code here
 
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+};
+
+function logAttendeeName(attendee) {
+  console.log(attendee.name);
+}
+
+function logTicketPrice(attendee) {
+  console.log(attendee.ticketPrice);
+}
+
+function updateTicketType(attendee, newType) {
+  attendee.ticketType = newType;
+}
+
+function updateTicketPrice(attendee, newPrice) {
+  attendee.ticketPrice = newPrice;
+}
+
+function removeEventProperty(attendee) {
+  delete attendee.event;
+}
+
+function addCheckedInProperty(attendee) {
+  attendee.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
